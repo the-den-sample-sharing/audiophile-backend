929f9dd413af1472b0b35d70e8dc90b18bb2af73
Rio Edwards <rioredwards@gmail.com>
2023-02-13 15:53:19 -0800

    Add SQL to setup users, profiles, sample_packs and profiles_sample_packs tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,22 +3,63 @@
 
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS profiles;
-
+DROP TABLE IF EXISTS profiles_sample_packs; -- join table
+DROP TABLE IF EXISTS sample_packs;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email VARCHAR NOT NULL,
   password_hash VARCHAR NOT NULL
-  
-  
 );
 
 CREATE TABLE profiles (
-  user_id BIGINT,
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT NOT NULL,
   username VARCHAR,
   first_name VARCHAR,
   last_name VARCHAR,
-  FOREIGN KEY(user_id) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE TABLE sample_packs (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR NOT NULL
+);
+
+CREATE TABLE profiles_sample_packs (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  profile_id BIGINT,
+  sample_pack_id BIGINT,
+  FOREIGN KEY (profile_id) REFERENCES profiles(id),
+  FOREIGN KEY (sample_pack_id) REFERENCES sample_packs(id)
+);
+
+INSERT INTO
+  users (email, password_hash)
+VALUES
+  ('rio@example.com', 'notarealpasswordhash'),
+  ('kyle@example.com', 'notarealpasswordhash'),
+  ('grey@example.com', 'notarealpasswordhash');
+
+INSERT INTO
+  profiles (user_id, username, first_name, last_name)
+VALUES
+  (1, 'rio_username', 'Rio', 'Edwards' ),
+  (2, 'kyle_username', 'Kyle', 'McCall' ),
+  (3, 'grey_username', 'Grey', 'Whithers' );
+
+INSERT INTO sample_packs (name)
+VALUES
+  ('riosamplepack1'),
+  ('riosamplepack2'),
+  ('kylesamplepack1'),
+  ('greysamplepack');
+
+INSERT INTO profiles_sample_packs (profile_id, sample_pack_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 3),
+    (3, 4);
 
 
